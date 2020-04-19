6c7a8fda9543e484755b324769b5c2b50801381f
Thibault Leroy <thibault.leroy.1@etudiant.univ-rennes1.fr>
2020-04-19 14:19:00 +0200

    types with env variables

--- front/spotifine/src/environments/set-env.ts
+++ front/spotifine/src/environments/set-env.ts
@@ -4,15 +4,27 @@ const targetPath = './../../src/environments/environment.ts';
 // Load node modules
 import {env} from '../../../../lib/env'
 // `environment.ts` file structure
-const envConfigFile = `export const environment = {
-   SF_BACKEND_ADDR_DEV: '${env.SF_BACKEND_ADDR_DEV}',
-   SF_BACKEND_ADDR_PROD: '${env.SF_BACKEND_ADDR_PROD}',
-   SF_BACKEND_PORT: ${env.SF_BACKEND_PORT},
-   SPOTIFY_CLIENT_ID: '${env.SPOTIFY_CLIENT_ID}',
-   SPOTIFY_CLIENT_SECRET: '${env.SPOTIFY_CLIENT_SECRET}',
-   SPOTIFY_API_URI: '${env.SPOTIFY_API_URI}'
-};
-`;
+// const envConfigFile = `export const environment = {
+//    SF_BACKEND_ADDR_DEV: '${env.SF_BACKEND_ADDR_DEV}',
+//    SF_BACKEND_ADDR_PROD: '${env.SF_BACKEND_ADDR_PROD}',
+//    SF_BACKEND_PORT: ${env.SF_BACKEND_PORT},
+//    SPOTIFY_CLIENT_ID: '${env.SPOTIFY_CLIENT_ID}',
+//    SPOTIFY_CLIENT_SECRET: '${env.SPOTIFY_CLIENT_SECRET}',
+//    SPOTIFY_API_URI: '${env.SPOTIFY_API_URI}'
+// };
+// `;
+
+const make_str_env = (env: any) => {
+    let res = '{';
+    for (const property in env) {
+        (typeof env[property] === "string" ? res +=`${property}: '${env[property]}',\r`: res +=`${property}: ${env[property]}\r,`);
+    }
+    console.log('result: ' + res)
+    return res.substring(0, res.length -2) + '}';
+}
+console.log('value', make_str_env(env));
+
+const envConfigFile = 'export const environment = ' + make_str_env(env) + ';';
 
 console.log('The file `environment.ts` will be written with the following content: \n');
 console.log(envConfigFile);
--- lib/env.ts
+++ lib/env.ts
@@ -6,7 +6,7 @@ config({ path: resolve(__dirname, "../../../.env") });
 export const env = {
     SF_BACKEND_ADDR_DEV: process.env.SF_BACKEND_ADDR_DEV,
     SF_BACKEND_ADDR_PROD: process.env.SF_BACKEND_ADDR_PROD,
-    SF_BACKEND_PORT: process.env.SF_BACKEND_PORT,
+    SF_BACKEND_PORT: parseInt(process.env.SF_BACKEND_PORT || ''),
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
     SPOTIFY_API_URI: process.env.SPOTIFY_API_URI
