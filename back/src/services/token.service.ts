38c099ff523b2e8baa001c91a8d308b26ea3ee04
Thibault Leroy <thibault.leroy.1@etudiant.univ-rennes1.fr>
2020-05-01 17:03:57 +0200

    update and refacto lib & back

--- back/src/services/token.service.ts
+++ back/src/services/token.service.ts
@@ -1,6 +1,5 @@
 import {credentials} from '../configs/spotify.config'
-import "spotify-web-api-node";
-import SpotifyWebApi from "spotify-web-api-node";
+const SpotifyWebApi = require("spotify-web-api-node");
 export const refreshAccessToken = async(refresh_token: string): Promise<any> => {
     const spotifyApi = new SpotifyWebApi(credentials);
     spotifyApi.setRefreshToken(refresh_token);
